1b67bc99f0a8ad482278cb646ccd7704af577782
Sohee <sohee3480@gmail.com>
2021-04-17 11:39:06 +0900

    버튼 누르면 로그인 로직 구현(토스트 만들기)

--- app/src/main/java/com/example/loginkotlintest_sohee/MainActivity.kt
+++ app/src/main/java/com/example/loginkotlintest_sohee/MainActivity.kt
@@ -11,17 +11,34 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        var inputId = idEdt.text
+        loginBtn.setOnClickListener {
 
-        var inputPassword = passwordEdt.text
+//            아이디 입력 값, 비번 입력 값을 각각의 변수에 저장
+
+            val inputId = idEdt.text.toString()
+            val inputPassword = passwordEdt.text.toString()
+
+//            두개의 변수에 들어있는 값 검사 (맞는 아이디 / 비번)
+
+//            맞을 때 -> 틀릴 때 분기처리(IF문 활용)
 
 
-        okTxt.setOnClickListener {
+//            id도 맞고, 비번도 맞을 때 => 관리자
+            if(inputId == "admin@test.com" && inputPassword == "qwer"){
+
+//                둘 다 맞으면 => 관리자 입니다. 토스트
+
+                Toast.makeText(this, "관리자입니다.", Toast.LENGTH_SHORT).show()
+        }
+            else {
+//                아이디나 비번 둘 중에 하나라도 틀리면 => 로그인 실패 토스트
+
+                Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+
+            }
 
 
 
-
-            Toast.makeText(this, "관리자입니다.", Toast.LENGTH_SHORT).show()
         }
     }
 }
